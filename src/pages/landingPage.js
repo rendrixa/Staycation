f5b0f0d53f72c1157afe6026f275d480f7942b8d
Rendrix <rendrixa@gmail.com>
2021-07-05 22:18:24 +0700

    Setup
    - Add Bootstrap
    - Setting file

---
+++ src/pages/landingPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import moduleName from "elements/button";
+
+export default class landingPage extends Component {
+	render() {
+		return <div></div>;
+	}
+}
