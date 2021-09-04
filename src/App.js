f6bfeee706db8eb06a7113dab70e64b50a5ae3e7
Rendrix <rendrixa@gmail.com>
2021-09-04 09:41:57 +0700

    enter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
 import Checkout from "pages/Checkout";
@@ -7,17 +7,15 @@ import Checkout from "pages/Checkout";
 import "./assets/scss/style.scss";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Switch>
-					<Route exact path="/" component={LandingPage} />
-					<Route exact path="/properties/:id" component={DetailsPage} />
-					<Route exact path="/checkout" component={Checkout} />
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route exact path="/checkout" component={Checkout} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,36 +1,37 @@
 import React, { Component } from "react";
+
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
-
-import landingPage from "json/landingPage.json";
 import Category from "parts/Category";
 import Testimony from "parts/Testimony";
 import Footer from "parts/Footer";
 
-export default class LandingPage extends Component {
-	constructor(props) {
-		super(props);
-		this.refMostPicked = React.createRef();
-	}
-	componentDidMount() {
-		window.title = "PeachyJelly | Home";
-		window.scrollTo(0, 0, "smooooooooth");
-	}
+import landingPageData from "json/landingPage.json";
 
-	render() {
-		return (
-			<>
-				<Header {...this.props} />
-				<Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
-				<MostPicked
-					refMostPicked={this.refMostPicked}
-					data={landingPage.mostPicked}
-				/>
-				<Category data={landingPage.categories} />
-				<Testimony data={landingPage.testimonial} />
-				<Footer />
-			</>
-		);
-	}
+export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+  componentDidMount() {
+    window.title = "PeachyJelly | Home";
+    window.scrollTo(0, 0, "smooooooooth");
+  }
+
+  render() {
+    return (
+      <>
+        <Header {...this.props} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPageData.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPageData.mostPicked}
+        />
+        <Category data={landingPageData.categories} />
+        <Testimony data={landingPageData.testimonial} />
+        <Footer />
+      </>
+    );
+  }
 }
